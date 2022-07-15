a9af27cb5b05f121abc9da127c1705a19264181a
Ninjaruss <russ081999@gmail.com>
2022-07-14 21:21:33 -0700

    Added initial database template.

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectDB = require('./config/db');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+connectDB();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
