2808315f71293283d6d4b81006fcd184441f936e
Ninjaruss <russ081999@gmail.com>
2022-11-05 14:27:05 -0700

    Backend initial.

--- application/backend/app.js
+++ application/backend/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const connectDB = require('./config/db');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,8 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-connectDB();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
