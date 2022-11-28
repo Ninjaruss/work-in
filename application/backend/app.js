2dac6724a36f80b08354b400d0cfeea4b2d06f51
Ninjaruss <russ081999@gmail.com>
2022-11-27 22:07:04 -0800

    ugh deployment dir

--- application/backend/app.js
+++ application/backend/app.js
@@ -33,8 +33,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+ });
 
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+// app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
