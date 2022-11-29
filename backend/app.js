d67d3a153dc694afb917aad051612ab06edec49c
Ninjaruss <russ081999@gmail.com>
2022-11-28 23:24:38 -0800

    Removed application from build path

--- backend/app.js
+++ backend/app.js
@@ -23,11 +23,11 @@ app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
 app.use(
-  express.static(path.join(__dirname, "../application/frontend/build"))
+  express.static(path.join(__dirname, "../frontend/build"))
 )
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../application/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 /*
