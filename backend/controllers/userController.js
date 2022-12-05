fac77ed58f34f37e6ee0d608449c9e932d30a552
Ninjaruss <russ081999@gmail.com>
2022-12-04 22:47:29 -0800

    Formatting updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,84 +8,84 @@ const User = require('../models/userModel')
 // @route POST /api/users/register
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { first_name, last_name, email, password } = req.body
-  
-    if (!first_name || !last_name || !email || !password) {
-      res.status(400)
-      throw new Error('Please add all fields')
-    }
-  
-    // Check if user exists
-    const userExists = await User.findOne({ email })
-  
-    if (userExists) {
-      res.status(400)
-      throw new Error('User already exists')
-    }
-  
-    // Hash password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-  
-    // Create user
-    const user = await User.create({
-      first_name,
-      last_name,
-      email,
-      password: hashedPassword,
+  const { first_name, last_name, email, password } = req.body
+
+  if (!first_name || !last_name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    first_name,
+    last_name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      token: generateToken(user._id),
     })
-  
-    if (user) {
-      res.status(201).json({
-        _id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        token: generateToken(user._id),
-      })
-    } else {
-      res.status(400)
-      throw new Error('Invalid user data')
-    }
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
   })
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body
 
-    // Check for user email
-    const user = await User.findOne({ email })
+  // Check for user email
+  const user = await User.findOne({ email })
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-        res.json({
-        _id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        token: generateToken(user._id),
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid credentials')
-    }
+  if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+      _id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      token: generateToken(user._id),
+      })
+  } else {
+      res.status(400)
+      throw new Error('Invalid credentials')
+  }
 })
 
 // @desc Get logged in user
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.status(200).json(req.user)
+  res.status(200).json(req.user)
 })
 
 // Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
-    })
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+  })
 }
 
 module.exports = {
-    registerUser, loginUser, getMe
+  registerUser, loginUser, getMe
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,13 +12,18 @@ const userSchema = mongoose.Schema(
     },
     email: {
       type: String,
-      required: [true, 'Please add an email'],
+      required: [false, 'Please add an email'],
       unique: true,
     },
     password: {
       type: String,
       required: [true, 'Please add a password'],
     },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+      ref: 'Organization'
+    }
   },
   {
     timestamps: true,
